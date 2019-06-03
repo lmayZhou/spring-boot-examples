8b733b98dea6876c9598fcb5834ae644103fb750
lmay <lmay@lmaye.com>
2019-06-03 20:45:21 +0800

    -- init docker compose.

--- spring-boot-docker/spring-boot-dockerfile/src/main/java/com/lmaye/spring/boot/dockerfile/SpringBootDockerfileApplication.java
+++ spring-boot-docker/spring-boot-dockerfile/src/main/java/com/lmaye/spring/boot/dockerfile/SpringBootDockerfileApplication.java
@@ -7,10 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootDockerfileApplication {
 
     public static void main(String[] args) {
-        try {
-            SpringApplication.run(SpringBootDockerfileApplication.class, args);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        SpringApplication.run(SpringBootDockerfileApplication.class, args);
     }
 }

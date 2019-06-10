4acebb6cf5d22d8e679ac1fe1c1581f7e6c0b985
lmay <lmay@lmaye.com>
2019-06-10 18:50:10 +0800

    - 添加测试方法.

--- spring-boot-docker/spring-boot-dockerfile/src/main/java/com/lmaye/spring/boot/dockerfile/controller/DockerController.java
+++ spring-boot-docker/spring-boot-dockerfile/src/main/java/com/lmaye/spring/boot/dockerfile/controller/DockerController.java
@@ -1,7 +1,11 @@
 package com.lmaye.spring.boot.dockerfile.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.lmaye.examples.common.common.Response;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * -- DockerController
@@ -21,5 +25,37 @@ public class DockerController {
     @GetMapping("/")
     public String index() {
         return "Hello Docker !";
+    }
+
+    /**
+     * 登陆
+     *
+     * @param username 用户名
+     * @param password 密码
+     * @return Response
+     */
+    @PostMapping("/login")
+    public Response login(@RequestParam String username, @RequestParam String password) throws InterruptedException {
+        Thread.sleep(70 * 1000);
+        if(!Objects.equals("root", username) || !Objects.equals("root", password)) {
+            return Response.failed("用户名或密码错误！");
+        }
+        Map<String, Object> result = new HashMap<>(1);
+        result.put("token", username + ":" + password);
+        return Response.success(result);
+    }
+
+    /**
+     * 测试方法
+     *
+     * @param token token
+     * @return Response
+     */
+    @GetMapping("/test/{token}")
+    public Response test(@PathVariable String token) {
+        if(!Objects.equals("root:root", token)) {
+            return Response.failed("无效Token！");
+        }
+        return Response.success(token);
     }
 }

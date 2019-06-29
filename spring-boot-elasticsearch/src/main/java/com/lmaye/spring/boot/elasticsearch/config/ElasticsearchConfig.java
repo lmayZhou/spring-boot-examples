3b56fcfc63a32081388873a7b1ad140f731f397b
lmay <lmay@lmaye.com>
2019-06-29 18:04:26 +0800

    - 生产各节点都设为主节点;

--- spring-boot-elasticsearch/src/main/java/com/lmaye/spring/boot/elasticsearch/config/ElasticsearchConfig.java
+++ spring-boot-elasticsearch/src/main/java/com/lmaye/spring/boot/elasticsearch/config/ElasticsearchConfig.java
@@ -26,12 +26,12 @@ public class ElasticsearchConfig {
      * @return TransportClient
      * @throws UnknownHostException 未知主机异常
      */
-    @Bean
+    /*@Bean
     public TransportClient transportClient() throws UnknownHostException {
         return new PreBuiltXPackTransportClient(Settings.builder()
             .put("cluster.name", "es-cluster").put("xpack.security.user", "elastic:123456").build())
             .addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.0.189"), 9300))
             .addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.0.189"), 9301))
             .addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.0.189"), 9302));
-    }
+    }*/
 }

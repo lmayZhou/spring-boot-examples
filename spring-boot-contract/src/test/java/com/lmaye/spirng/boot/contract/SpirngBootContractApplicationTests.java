63eb4b1dff16b669d9bc9f6b329322c1f96dad78
lmay <lmay@lmaye.com>
2020-06-08 19:57:08 +0800

    -- init Contract Project.

---
+++ spring-boot-contract/src/test/java/com/lmaye/spirng/boot/contract/SpirngBootContractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmaye.spirng.boot.contract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpirngBootContractApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

ad9cc5b052fa8fef1b9785f0175a81eab538be5b
lmay <lmay@lmaye.com>
2021-05-16 17:26:42 +0800

    feat: 工作流.

--- spring-boot-activiti/src/main/java/com/lmaye/activiti/ActivitiApplication.java
+++ spring-boot-activiti/src/main/java/com/lmaye/activiti/ActivitiApplication.java
@@ -3,6 +3,15 @@ package com.lmaye.activiti;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * -- Activiti Application
+ *
+ * @author lmay.Zhou
+ * @date 2021/5/16 17:25
+ * @qq 379839355
+ * @email lmay@lmaye.com
+ * @since JDK1.8
+ */
 @SpringBootApplication
 public class ActivitiApplication {
     public static void main(String[] args) {

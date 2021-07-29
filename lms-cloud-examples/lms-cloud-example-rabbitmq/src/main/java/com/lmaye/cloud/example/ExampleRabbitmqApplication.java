aeba35cb7344c80665905852be262213e2d5ec1c
lmay <lmay@lmaye.com>
2021-07-29 23:22:39 +0800

    feat: 新增MQ测试示例.

---
+++ lms-cloud-examples/lms-cloud-example-rabbitmq/src/main/java/com/lmaye/cloud/example/ExampleRabbitmqApplication.java
@@ -0,0 +1,19 @@
+package com.lmaye.cloud.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * -- Example Rabbitmq Application
+ *
+ * @author Lmay Zhou
+ * @date 2021/7/29 23:05
+ * @email lmay@lmaye.com
+ * @sine jdk1.8
+ */
+@SpringBootApplication
+public class ExampleRabbitmqApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleRabbitmqApplication.class, args);
+    }
+}
---
+++ lms-cloud-examples/lms-cloud-example-rabbitmq/src/test/java/com/lmaye/cloud/example/rabbitmq/ExampleRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmaye.cloud.example.rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleRabbitmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

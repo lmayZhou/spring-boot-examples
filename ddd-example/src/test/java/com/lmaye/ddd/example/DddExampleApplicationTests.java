9f1f6744cb6a87e659fe0a0f03b7edb6d6c90439
lmay <lmay@lmaye.com>
2021-11-08 18:17:21 +0800

    feat: TODO 领域驱动设计.

---
+++ ddd-example/src/test/java/com/lmaye/ddd/example/DddExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmaye.ddd.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

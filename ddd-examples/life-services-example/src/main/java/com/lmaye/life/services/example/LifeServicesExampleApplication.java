3933f04de15e4b407c7ec466bbdb714a8ad4dee2
lmay <lmay@lmaye.com>
2021-11-17 10:17:31 +0800

    feat: TODO 领域驱动设计.

---
+++ ddd-examples/life-services-example/src/main/java/com/lmaye/life/services/example/LifeServicesExampleApplication.java
@@ -0,0 +1,13 @@
+package com.lmaye.life.services.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeServicesExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifeServicesExampleApplication.class, args);
+    }
+
+}
---
+++ ddd-examples/life-services-example/src/test/java/com/lmaye/life/services/example/LifeServicesExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmaye.life.services.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeServicesExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

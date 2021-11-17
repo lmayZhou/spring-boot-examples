fbd4cf371b4992cac44d87494c208363d91002ef
lmay <lmay@lmaye.com>
2021-11-17 17:19:15 +0800

    feat: TODO 领域驱动设计.

---
+++ ddd-examples/eshop/eshop-user/src/main/java/com/lmaye/eshop/UserApplication.java
@@ -0,0 +1,19 @@
+package com.lmaye.eshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * -- User Application
+ *
+ * @author Lmay Zhou
+ * @date 2021/11/17 15:54
+ * @email lmay@lmaye.com
+ * @since JDK1.8
+ */
+@SpringBootApplication
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}

850c9dda15e7c3fc4dfe668b6885894dd28593a9
lmay <lmay@lmaye.com>
2021-11-20 07:40:59 +0800

    feat: TODO 领域驱动设计.

---
+++ ddd-examples/eshop/eshop-user/user-api/src/main/java/com/lmaye/eshop/user/api/UserApiFeign.java
@@ -0,0 +1,12 @@
+package com.lmaye.eshop.user.api;
+
+/**
+ * -- User Api Feign
+ *
+ * @author Lmay Zhou
+ * @date 2021/11/19 17:55
+ * @email lmay@lmaye.com
+ * @since JDK1.8
+ */
+public interface UserApiFeign {
+}
--- ddd-examples/eshop/eshop-user/user-service/src/test/java/com/lmaye/eshop/user/service/UserApplicationTests.java
+++ ddd-examples/eshop/eshop-user/user-service/src/test/java/com/lmaye/eshop/user/service/UserApplicationTests.java
@@ -1,4 +1,4 @@
-package com.lmaye.eshop.user;
+package com.lmaye.eshop.user.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

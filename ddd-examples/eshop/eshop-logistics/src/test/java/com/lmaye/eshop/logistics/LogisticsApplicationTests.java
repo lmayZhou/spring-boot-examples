8e95322f305c6bdd420388c7c53b15691041b48f
lmay <lmay@lmaye.com>
2021-12-01 23:08:43 +0800

    feat: TODO 领域驱动设计.

--- ddd-examples/eshop/eshop-logistics/src/test/java/com/lmaye/eshop/logistics/LogisticsApplicationTests.java
+++ ddd-examples/eshop/eshop-logistics/src/test/java/com/lmaye/eshop/logistics/LogisticsApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EshopLogisticsApplicationTests {
+class LogisticsApplicationTests {
 
     @Test
     void contextLoads() {
